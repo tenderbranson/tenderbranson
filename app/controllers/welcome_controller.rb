c4620124a51a28f789c2c36740caf6e66c1b857a
Tender Branson <tenderbranson79@gmail.com>
2008-12-19 01:47:16 -0800

    added sample action with sleep

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,9 @@ class WelcomeController < ApplicationController
   def index
     render :text => "Hello<br />#{Time.now}"
   end
+
+  def sleep
+    sleep(params[:time] || 2)
+    render :text => 'done'
+  end
 end
